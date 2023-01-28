d889ceec63a1dfed2c47803521d2cd4511178c3f
SamsonMokaya <mokayasamson@gmail.com>
2023-01-28 10:33:08 +0300

    Creating the api post request using futures

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:untitled2/constant.dart';
+import 'package:http/http.dart' as http;
 
 import 'model.dart';
 
@@ -42,6 +46,32 @@ class _ChatPageState extends State<ChatPage> {
   void initState(){
     super.initState();
     isLoading = false;
+  }
+
+  Future<String> generateRensponse(String prompt) async{
+    final apiKey = apiSecretKey;
+    var url = Uri.https("api.openai.com", "v1/completions");
+    final response = await http.post(
+      url,
+      headers: {
+        'Content-Type': 'application/json',
+        "Authorization": "Bearer $apiKey"
+      },
+
+      body: json.encode({
+        "model": "text-davinci-003",
+        "prompt": prompt,
+        'temperature': 0,
+        'max_tokens': 2000,
+        'top_p': 1,
+        'frequency_penalty': 0.0,
+        'presence_penalty': 0.0,
+      }),
+    );
+
+    //decode the response
+    Map<String, dynamic> newresponse = jsonDecode(response.body);
+    return newresponse['choices'][0]['text'];
   }
 
 
