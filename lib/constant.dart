95ed37752c215ded68365a412817b754dbdff900
SamsonMokaya <mokayasamson@gmail.com>
2023-01-28 14:27:45 +0300

    Working chatGPT flutter app

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,1 +1,4 @@
-const apiSecretKey = "---- Place secret key here !! ----";
+const apiSecretKey = "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";
+
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:untitled2/constant.dart';
 import 'package:http/http.dart' as http;
-
+import 'constant.dart';
 import 'model.dart';
 
 void main() {
@@ -11,13 +10,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chat GPT',
+    return const MaterialApp(
       home: ChatPage(),
       debugShowCheckedModeBanner: false,
     );
@@ -26,120 +23,152 @@ class MyApp extends StatelessWidget {
 
 const backgroundColor = Color(0xff343541);
 const botBackgroundColor = Color(0xff444654);
-const botBackgroundColor2 = Color(0xff404d4d);
 
 class ChatPage extends StatefulWidget {
-  const ChatPage({Key? key}) : super(key: key);
+  const ChatPage({super.key});
 
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
 
-class _ChatPageState extends State<ChatPage> {
+Future<String> generateResponse(String prompt) async {
+  const apiKey = apiSecretKey;
 
-  late bool isLoading;
-  TextEditingController _textController = TextEditingController();
+  var url = Uri.https("api.openai.com", "/v1/completions");
+  final response = await http.post(
+    url,
+    headers: {
+      'Content-Type': 'application/json',
+      "Authorization": "Bearer $apiKey"
+    },
+    body: json.encode({
+      "model": "text-davinci-003",
+      "prompt": prompt,
+      'temperature': 0.4,
+      'max_tokens': 2000,
+      'top_p': 1,
+      'frequency_penalty': 0.0,
+      'presence_penalty': 0.0,
+    }),
+  );
+
+  // Do something with the response
+  Map<String, dynamic> newresponse = jsonDecode(response.body);
+
+  return newresponse['choices'][0]['text'];
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final _textController = TextEditingController();
   final _scrollController = ScrollController();
   final List<ChatMessage> _messages = [];
+  late bool isLoading;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     isLoading = false;
   }
 
-  Future<String> generateRensponse(String prompt) async{
-    final apiKey = apiSecretKey;
-    var url = Uri.https("api.openai.com", "v1/completions");
-    final response = await http.post(
-      url,
-      headers: {
-        'Content-Type': 'application/json',
-        "Authorization": "Bearer $apiKey"
-      },
-
-      body: json.encode({
-        "model": "text-davinci-003",
-        "prompt": prompt,
-        'temperature': 0,
-        'max_tokens': 2000,
-        'top_p': 1,
-        'frequency_penalty': 0.0,
-        'presence_penalty': 0.0,
-      }),
-    );
-
-    //decode the response
-    Map<String, dynamic> newresponse = jsonDecode(response.body);
-    return newresponse['choices'][0]['text'];
-  }
-
-
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 100,
-          title: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              "OpenAi's ChatGPT",
-              maxLines: 2,
-              textAlign: TextAlign.center,
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 50,
+        title: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text(
+            "OpenAI's ChatGPT",
+            maxLines: 2,
+            textAlign: TextAlign.center,
           ),
-          backgroundColor: botBackgroundColor,
         ),
-
-       backgroundColor: backgroundColor,
-
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-
-              //chat body
-              Expanded(
-                  child: _buildList(),
+        backgroundColor: botBackgroundColor,
+      ),
+      backgroundColor: backgroundColor,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: _buildList(),
+            ),
+            Visibility(
+              visible: isLoading,
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: CircularProgressIndicator(
+                  color: Colors.white,
+                ),
               ),
-
-              // Loads or rotates to show that the bot is thinking
-              Visibility(
-                visible: isLoading,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: CircularProgressIndicator(
-                      color: Colors.white,
-                    ),
-                  ),
-              ),
-
-              Row(
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
                 children: [
-
-                  //input filed
-                  _builtInput(),
-
-                  //submit button
+                  _buildInput(),
                   _buildSubmit(),
                 ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-
       ),
     );
   }
 
-  Expanded _builtInput(){
+  Widget _buildSubmit() {
+    return Visibility(
+      visible: !isLoading,
+      child: Container(
+        color: botBackgroundColor,
+        child: IconButton(
+          icon: const Icon(
+            Icons.send_rounded,
+            color: Color.fromRGBO(142, 142, 160, 1),
+          ),
+          onPressed: () async {
+            setState(
+                  () {
+                _messages.add(
+                  ChatMessage(
+                    text: _textController.text,
+                    chatMessageType: ChatMessageType.user,
+                  ),
+                );
+                isLoading = true;
+              },
+            );
+            var input = _textController.text;
+            _textController.clear();
+            Future.delayed(const Duration(milliseconds: 50))
+                .then((_) => _scrollDown());
+            generateResponse(input).then((value) {
+              setState(() {
+                isLoading = false;
+                _messages.add(
+                  ChatMessage(
+                    text: value,
+                    chatMessageType: ChatMessageType.bot,
+                  ),
+                );
+              });
+            });
+            _textController.clear();
+            Future.delayed(const Duration(milliseconds: 50))
+                .then((_) => _scrollDown());
+          },
+        ),
+      ),
+    );
+  }
+
+  Expanded _buildInput() {
     return Expanded(
       child: TextField(
         textCapitalization: TextCapitalization.sentences,
-        style: TextStyle(color: Colors.white),
+        style: const TextStyle(color: Colors.white),
         controller: _textController,
-        decoration: InputDecoration(
+        decoration: const InputDecoration(
           fillColor: botBackgroundColor,
           filled: true,
           border: InputBorder.none,
@@ -152,92 +181,86 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 
-  Widget _buildSubmit(){
-    return Visibility(
-      visible: !isLoading,
-      child: Container(
-        color: botBackgroundColor,
-        child: IconButton(
-          icon: Icon(
-              Icons.send_rounded,
-            color: Color.fromRGBO(142, 142, 160, 1),
-          ),
-          onPressed: (){},
-        ),
-      ),
+  ListView _buildList() {
+    return ListView.builder(
+      controller: _scrollController,
+      itemCount: _messages.length,
+      itemBuilder: (context, index) {
+        var message = _messages[index];
+        return ChatMessageWidget(
+          text: message.text,
+          chatMessageType: message.chatMessageType,
+        );
+      },
     );
   }
 
-  ListView _buildList(){
-    return ListView.builder(
-      itemCount: _messages.length,
-      controller: _scrollController,
-      itemBuilder: ((context, index){
-        var message = _messages[index];
-          return ChatMessageWidget(
-            text: message.text,
-            chatMessageType: message.chatMessageType,
-          );
-      }),
+  void _scrollDown() {
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut,
     );
   }
 }
 
 class ChatMessageWidget extends StatelessWidget {
+  const ChatMessageWidget(
+      {super.key, required this.text, required this.chatMessageType});
+
   final String text;
   final ChatMessageType chatMessageType;
-
-  const ChatMessageWidget({super.key, required this.text, required this.chatMessageType});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      padding: EdgeInsets.all(16.0),
-      color: chatMessageType == ChatMessageType.bot ? botBackgroundColor: backgroundColor,
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      padding: const EdgeInsets.all(16),
+      color: chatMessageType == ChatMessageType.bot
+          ? botBackgroundColor
+          : backgroundColor,
       child: Row(
-        children: [
-          chatMessageType == ChatMessageType.bot ? Container(
-            margin: EdgeInsets.only(right: 16),
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          chatMessageType == ChatMessageType.bot
+              ? Container(
+            margin: const EdgeInsets.only(right: 16.0),
             child: CircleAvatar(
-              child: Image.asset(
-                  'assets/2.jpeg',
-                color: Colors.white,
-                scale: 1.5,
+              backgroundImage: AssetImage(
+                'assets/2.jpeg',
               ),
             ),
-          ): Container(
-            margin: EdgeInsets.only(right: 16),
-            child: CircleAvatar(
-                child: Icon(Icons.person),
-            )
+          )
+              : Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: const CircleAvatar(
+              child: Icon(
+                Icons.person,
+              ),
+            ),
           ),
           Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(8)
-                      ),
-                    ),
-                    child: Text(
-                      text,
-                      style: Theme.of(context)
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  ),
+                  child: Text(
+                    text,
+                    style: Theme.of(context)
                         .textTheme
                         .bodyLarge
                         ?.copyWith(color: Colors.white),
-                    ),
-                  )
-                ],
-          ))
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
-
-
